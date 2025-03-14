58dd449b8597489fa74e8f0c893879e7761feb84
Osarikas <yastruckov@yandex.ru>
2025-03-14 23:02:15 +0300

    decorator

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+def binaryEncoder(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if isinstance(result, str):
+            return ' '.join(format(ord(c), '08b') for c in result)
+        return result
+    return wrapper
+
+def binaryDecoder(func):
+    def wrapper(*args, **kwargs):
+        binary_string = func(*args, **kwargs)
+        return ''.join(chr(int(b, 2)) for b in binary_string.split())
+    return wrapper
+
+@binaryEncoder
+def getMessage(binaryString):
+    return binaryString
+
+@binaryDecoder
+def getBinaryMessage(string):
+    return string
+
 encodeType = input("Enter encode or decode: ")
-resultString = ''
-if encodeType == 'encode':
-    inputString = input("Enter a binary string: ")
-    for i in range(0, len(inputString), 8):
-        byte = inputString[i:i+8]
-        charCode = int(byte, 2)
-        resultString += chr(charCode)
-    print(resultString)
-elif encodeType == 'decode':
-    inputString = input("Enter a string: ")
-
-    for char in inputString:
-        charCode = ord(char)
-        resultString += format(charCode, '08b')
-
-    print(resultString)
+if encodeType == "encode":
+    print(getMessage(input("Enter a string: ")))
+elif encodeType == "decode":
+    print(getBinaryMessage(input("Enter a binary string: ")))
 else:
     print("Invalid input")
